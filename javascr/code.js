3f85e9ed54ebc9017e1301cfe2f972b48d4e4a38
JoseGabriel2402 <gomesaraujo242@gmail.com>
2025-02-11 17:51:55 -0300

    ui added and console logs removed

--- javascr/code.js
+++ javascr/code.js
@@ -1,46 +1,133 @@
-let Humanpoints= 0
-let PCpoints= 0
-//gerar escolha do computador
-function getComputerChoice(){
- max= 4
- min=1
- let PCchoice= Math.random() * (max - min) + min
- if (PCchoice>= 3)
-   {PCchoice= "scissors"
-   }else{ 
-    if(PCchoice>= 2)
-    {PCchoice= "paper"
-    }else{
-    PCchoice= "rock"}
+let humanPoints= 0
+let pcPoints= 0
+
+//decidir limite de pontos para vitoria,apontar vencedor e resetar placar após vitoria
+function score(){
+let humanPointsDisplay= document.getElementById("playerScore")
+let pcPointsDisplay= document.getElementById("computerScore")
+let winnerDisplay= document.getElementById("match result")
+let winner=""
+humanPointsDisplay.textContent="Player score: "+humanPoints
+pcPointsDisplay.textContent="Computer score: "+pcPoints
+//se pcPoints ou humanPoints >5
+if (pcPoints==5 || humanPoints==5) {
+//declarar o maior pontuador vencedor
+   if (humanPoints>pcPoints) {
+      winner="PLAYER"
+   }else{
+      winner="COMPUTER"
    }
- console.log("Computer:",PCchoice)
- return PCchoice
-}
-//receber escolha do jogador 
-function getHumanChoice(){
-//validar escolha
- let valid_answers= ["rock","paper","scissors"]
- let Hchoice= prompt("rock,paper or scissors?","rock").toLowerCase()
- if (!valid_answers.includes(Hchoice))
-    {Hchoice= "Invalid answer!"}
- console.log("Human:",Hchoice)
- return Hchoice
- }
-
-
-//repetir Playround() para x rodadas e mostrar pontos
-function playGame(){
-let round= 0
-let finalResult=""
-while (round< 5) {
-   Playround()
-   ++round
-   console.log("round:",round)
-}
+   winnerDisplay.textContent=winner+" WINS!"
+//e resetar ambos os valores
+   humanPoints=0
+   pcPoints=0
 }
 
-//comparar resultados,decidir ganhador da rodada e pontuar
-function Playround(){
+}
+
+function compare(hChoice,pcChoice) {
+//comparar escolhas e incrementar pontos dependendo do resultado
+   let result= ""
+   let roundResultDisplay=document.getElementById("roundResult")
+   if (hChoice=== pcChoice)
+      {result = "Tie!"
+      }else{
+      if ((hChoice=== "rock" && pcChoice=== "scissors")
+         ||
+            (hChoice=== "scissors" && pcChoice=== "paper")
+         || 
+            (hChoice=== "paper" && pcChoice==="rock"))  
+
+            {result= "Player scores!"
+             ++humanPoints
+            }
+      else{result= "Computer scores!"
+           ++pcPoints
+         }}
+   roundResultDisplay.textContent=result
+   score()      
+   return
+}
+//receber escolha do jogador e gerar escolha do computador
+function getChoices(){
+//escolha do jogador
+   let hChoice= "";
+   let playerChoiceDisplay= document.getElementById("playerChoice")
+   let computerChoiceDisplay= document.getElementById("computerChoice")
+   let buttons= document.getElementById("choicesContainer");
+   buttons.addEventListener("click", (event)=>{
+      let target= event.target
+      switch (target.id){
+         case "rock":
+            hChoice= target.id;
+            break;
+         case "scissors":
+            hChoice= target.id;
+            break;
+         case "paper":
+            hChoice= target.id;
+            break;
+         }
+//escolha do computador
+   const max= 4;
+   const min= 1;
+   let pcChoice= Math.random() * (max - min) + min;
+      if (pcChoice>= 3){
+         pcChoice= "scissors";
+         }else{ 
+            if(pcChoice>= 2)
+            {pcChoice= "paper";
+            }else{
+            pcChoice= "rock";
+         }}
+//invocar função de comparação
+         compare(pcChoice,hChoice)
+         playerChoiceDisplay.textContent="Player choice: "+hChoice
+         computerChoiceDisplay.textContent="Computer choice: "+pcChoice 
+          return
+      })
+
+   }
+getChoices()
+
+
+
+console.log(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function playGame(){
+//let round= 0
+//let finalResult=""
+//while (round< 5) {
+//   Playround()
+//  ++round
+//   console.log("round:",round)
+//}
+//}
+
+/* function Playround(){
 
 let Hchoice= getHumanChoice()
 let PCchoice= getComputerChoice()
@@ -66,3 +153,4 @@ console.log("computer points:",PCpoints)
 }
 playGame()
 
+ */
